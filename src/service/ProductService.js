d920422bafbc4596e4c7868328980ce5493a1459
Marlon <marlon.divino@gmail.com>
2022-08-31 20:59:25 -0300

    Initial version

--- src/service/ProductService.js
+++ src/service/ProductService.js
@@ -1,19 +1,16 @@
+import axios from 'axios';
+
 export class ProductService {
-  getProductsSmall() {
-    return fetch("data/products-small.json")
-      .then((res) => res.json())
-      .then((d) => d.data);
-  }
 
-  getProducts() {
-    return fetch("data/products.json")
-      .then((res) => res.json())
-      .then((d) => d.data);
-  }
+    getProductsSmall() {
+        return axios.get('assets/demo/data/products-small.json').then(res => res.data.data);
+    }
 
-  getProductsWithOrdersSmall() {
-    return fetch("data/products-orders-small.json")
-      .then((res) => res.json())
-      .then((d) => d.data);
-  }
+    getProducts() {
+        return axios.get('assets/demo/data/products.json').then(res => res.data.data);
+    }
+
+    getProductsWithOrdersSmall() {
+        return axios.get('assets/demo/data/products-orders-small.json').then(res => res.data.data);
+    }
 }
